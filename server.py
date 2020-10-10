83c5031af3dd92774e7b0f09afba613a48742191
Lydia Kajeckas <lydiakajeckas@gmail.com>
2020-10-10 15:33:33 -0700

    key validation

--- server.py
+++ server.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+import os
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def get():
-    return 'is this working'
+    return request.args.get('apiKey')
+
+def validate_key(provided_key):
+    if provided_key == null:
+        print('Key Not Found')
+        return False
+    if provided_key == os.environ.get('API_KEY'):
+        return True
+    else:
+        print('Key Incorrect')
+        return False
