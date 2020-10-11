a665a5108cbd514a6aca9b26a3b2bb2379e491c5
Lydia Kajeckas <lydiakajeckas@gmail.com>
2020-10-10 21:28:57 -0700

    updated server file, consolidated hypernym/hyponym function

--- server.py
+++ server.py
@@ -28,19 +28,20 @@ masc = ['active', 'adventurous', 'aggress', 'ambitio', 'analy', 'assert', 'athle
 
 
 def most_similar_subjective(txt):
+    rm_punc = re.sub(r'[^\w\s]', '', txt)
+    txt_list = rm_punc.split()
     suggestions = []
-    for word in masc:
-        if word in txt:
-            temp = (word, [])
-            for similar_word in model.most_similar(word):
-                if similar_word[0] not in masc:
-                    temp[1].append(similar_word[0])
-            suggestions.append(temp)
+    for word in txt_list:
+        for fragment in masc:
+            if fragment in word:
+                temp = (word, [])
+                for similar_word in model.most_similar(word):
+                    if fragment not in similar_word[0]:
+                        temp[1].append(similar_word[0])
+                suggestions.append(temp)
     return suggestions
 
-
-
-def hypernyms_subjective(txt):
+def hyper_hypo_nyms(txt):
     rm_punc = re.sub(r'[^\w\s]', '', txt)
     suggestions = []
     dictionary = {}
@@ -65,45 +66,19 @@ def hypernyms_subjective(txt):
                     for hypernym in wn.synset(sinset.name()).hypernyms():
                         if fragment not in hypernym.name().split('.')[0]:
                             temp[1].append(hypernym.name().split('.')[0])
-                suggestions.append(temp)
-    return suggestions
-
-
-
-def hyponyms_subjective(txt):
-    rm_punc = re.sub(r'[^\w\s]', '', txt)
-    suggestions = []
-    dictionary = {}
-    tokenized = sent_tokenize(rm_punc)
-    for words in tokenized:
-        word_lst = nltk.word_tokenize(words)
-        tagged = nltk.pos_tag(word_lst)
-    for item in tagged:
-        for fragment in masc:
-            if fragment in item[0]:
-                temp = (item[0], [])
-                dictionary[item[0]] = item[1]
-                if dictionary[item[0]].startswith('N'):
-                     pos = 'n'
-                elif dictionary[item[0]].startswith('J'):
-                    pos = 'a'
-                elif dictionary[item[0]].startswith('R'):
-                     pos = 'r'
-                elif dictionary[item[0]].startswith('V'):
-                    pos = 'v'
-                for sinset in wn.synsets(item[0], pos):
                     for hyponym in wn.synset(sinset.name()).hyponyms():
                         if fragment not in hyponym.name().split('.')[0]:
                             temp[1].append(hyponym.name().split('.')[0])
                 suggestions.append(temp)
     return suggestions
 
+
 @app.route("/")
 def get():
     provided_key = request.args.get('apiKey')
     if validate_key(provided_key):
         input = request.json
-        results = subjective_words(input['descript'])
+        results = most_similar_subjective(input['descript'])
         return jsonify(results)
     else:
         return 'API Key Missing/Invalid'
