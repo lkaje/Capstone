c2a4fa17017f03315857beb49b9faff7ee9463c1
Lydia Kajeckas <lydiakajeckas@gmail.com>
2020-10-11 21:36:59 -0700

    changed subtitle size

--- streamlitwebsite.py
+++ streamlitwebsite.py
@@ -111,6 +111,8 @@ def highlight(user_input):
     return tuple(working_list)
 
 
+st.title("recommend{her}")
+st.subheader("a tool to identify and adjust gendered language in job descriptions")
 user_input = st.text_area("Job Description", value='', height = 300)
 
 highlighted = highlight(user_input)
